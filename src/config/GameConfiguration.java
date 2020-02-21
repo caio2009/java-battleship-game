80c5f7c3e47cc8f9b15c7989ba2e2d3cec220c33
Caio Yukihiro <caioyukihiro@gmail.com>
2020-02-21 11:24:57 -0300

    Changing Class GameConfiguration
    Changed value of the const NUMBER_OF_ROWS to 10 and also the value of
    const NUMBER_OF_COLUMNS to 10.

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -2,7 +2,7 @@ package config;
 
 public class GameConfiguration {
 
-    public static final int NUMBER_OF_ROWS = 8;
-    public static final int NUMBER_OF_COLUMNS = 8;
+    public static final int NUMBER_OF_ROWS = 10;
+    public static final int NUMBER_OF_COLUMNS = 10;
 
 }
