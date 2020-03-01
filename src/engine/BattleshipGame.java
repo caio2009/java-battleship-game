f93e56ff6613969295a4500ed003e027225e43e7
Caio Yukihiro <caioyukihiro@gmail.com>
2020-03-01 15:20:03 -0300

    Change return of method checkBattleships
    Before, the method checkBattleships returned void. With this change,
    now the method return a Ship object.
    
    This change was did to display which ship type was sunk, when method
    checkBattleships identify a ship was sunk.

--- src/engine/BattleshipGame.java
+++ src/engine/BattleshipGame.java
@@ -36,7 +36,7 @@ public class BattleshipGame {
     }
 
     // Check if battleships were sunk, so add to sunkShips list
-    public void checkBattleships() {
+    public Ship checkBattleships() {
         Ship markedShip = null; // marked ship which was sunk
 
         for (Ship ship : ships) {
@@ -60,7 +60,9 @@ public class BattleshipGame {
         if (markedShip != null) {
             ships.remove(markedShip);
             sunkShips.add(markedShip);
+            return markedShip;
         }
+        return null;
     }
 
     public List<Ship> getShips() {
@@ -73,15 +75,24 @@ public class BattleshipGame {
 
     private void initShips() {
         // Instance for tests
-        Ship ship = new Ship(
+        Ship ship1 = new Ship(
                 ShipType.SUBMARINE,
+                Arrays.asList(
+                        new ShipPosition(new BattleshipPosition('c', 3).toPosition()/*, true*/),
+                        new ShipPosition(new BattleshipPosition('c', 4).toPosition()/*, true*/),
+                        new ShipPosition(new BattleshipPosition('c', 5).toPosition()/*, true*/)
+                )
+        );
+        Ship ship2 = new Ship(
+                ShipType.DESTROYER,
                 Arrays.asList(
                         new ShipPosition(new BattleshipPosition('a', 1).toPosition()/*, true*/),
                         new ShipPosition(new BattleshipPosition('b', 1).toPosition()/*, true*/),
                         new ShipPosition(new BattleshipPosition('c', 1).toPosition()/*, true*/)
                 )
         );
-        ships.add(ship);
+        ships.add(ship1);
+        ships.add(ship2);
     }
 
     private void initShipPositions() {
