a6f14886439bab9b50f2975adabb76a725bf28bd
Caio Yukihiro <caioyukihiro@gmail.com>
2020-03-05 10:14:14 -0300

    Change GameConfiguration class to test environment

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -4,10 +4,10 @@ public class GameConfiguration {
 
     public static final int NUMBER_OF_ROWS = 10;
     public static final int NUMBER_OF_COLUMNS = 10;
-    public static final int NUMBER_OF_CARRIERS = 1;
-    public static final int NUMBER_OF_BATTLESHIPS = 1;
+    public static final int NUMBER_OF_CARRIERS = 0;
+    public static final int NUMBER_OF_BATTLESHIPS = 0;
     public static final int NUMBER_OF_DESTROYERS = 2;
-    public static final int NUMBER_OF_SUBMARINES = 2;
-    public static final int NUMBER_OF_PATROL_BOATS = 3;
+    public static final int NUMBER_OF_SUBMARINES = 0;
+    public static final int NUMBER_OF_PATROL_BOATS = 0;
 
 }
